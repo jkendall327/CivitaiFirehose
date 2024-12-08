f7cf0d0c0fc31877659b8bfc9cc33b5f03d50660
jackkendall <jkendall3096@gmail.com>
2024-12-08 18:49:33 +0000

    Use extension methods for DI registrations

--- CivitaiFirehose/Program.cs
+++ CivitaiFirehose/Program.cs
@@ -1,40 +1,24 @@
-using System.Threading.Channels;
 using CivitaiFirehose;
 using CivitaiFirehose.Components;
-using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-
-var channel = Channel.CreateUnbounded<ImageModel>();
-builder.Services.AddSingleton(channel.Writer);
-builder.Services.AddSingleton(channel.Reader);
-
-builder.Services.Configure<CivitaiSettings>(builder.Configuration.GetSection(nameof(CivitaiSettings)));
-builder.Services.Configure<HydrusSettings>(builder.Configuration.GetSection(nameof(HydrusSettings)));
-
-builder.Services.AddHttpClient<CivitaiClient>();
-builder.Services.AddHttpClient<HydrusClient>((s, c) =>
-{
-    var opt = s.GetRequiredService<IOptions<HydrusSettings>>().Value;
-    c.BaseAddress = new(opt.BaseUrl);
-    c.DefaultRequestHeaders.Add("Hydrus-Client-API-Access-Key", opt.ApiKey);
+builder.Host.UseDefaultServiceProvider((context, options) => {
+    options.ValidateScopes = true;
+    options.ValidateOnBuild = true;
 });
 
-builder.Services.AddScoped<JsService>();
-builder.Services.AddSingleton<ICivitaiPoller, CivitaiPoller>();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-builder.Services.AddHostedService<CivitaiPollingBackgroundService>();
-builder.Services.AddHostedService<HydrusPusherBackgroundService>();
+builder.Services.AddCivitaiServices(builder.Configuration);
+builder.Services.AddHydrusServices(builder.Configuration);
+builder.Services.AddWebServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
---
+++ CivitaiFirehose/ServiceCollectionExtensions.cs
@@ -0,0 +1,45 @@
+using System.Threading.Channels;
+using Microsoft.Extensions.Options;
+
+namespace CivitaiFirehose;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCivitaiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var channel = Channel.CreateUnbounded<ImageModel>();
+        services.AddSingleton(channel.Writer);
+        services.AddSingleton(channel.Reader);
+
+        services.Configure<CivitaiSettings>(configuration.GetSection(nameof(CivitaiSettings)));
+            
+        // Civitai specific services
+        services.AddHttpClient<CivitaiClient>();
+        services.AddSingleton<ICivitaiPoller, CivitaiPoller>();
+        services.AddHostedService<CivitaiPollingBackgroundService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddHydrusServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<HydrusSettings>(configuration.GetSection(nameof(HydrusSettings)));
+            
+        services.AddHttpClient<HydrusClient>((s, c) =>
+        {
+            var opt = s.GetRequiredService<IOptions<HydrusSettings>>().Value;
+            c.BaseAddress = new(opt.BaseUrl);
+            c.DefaultRequestHeaders.Add("Hydrus-Client-API-Access-Key", opt.ApiKey);
+        });
+        services.AddHostedService<HydrusPusherBackgroundService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services.AddScoped<JsService>();
+
+        return services;
+    }
+}
