d8f4ff90f8f3eb9d139d39efebb1609c39572507
jackkendall <jkendall3096@gmail.com>
2024-12-08 15:59:12 +0000

    Get tag services before adding tags

--- CivitaiFirehose/HydrusClient.cs
+++ CivitaiFirehose/HydrusClient.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace CivitaiFirehose;
 
 public class HydrusClient(HttpClient client, ILogger<HydrusClient> logger)
@@ -5,25 +8,80 @@ public class HydrusClient(HttpClient client, ILogger<HydrusClient> logger)
     public async Task VerifyAccess()
     {
         var response = await client.GetAsync("verify_access_key");
-        
-        response.EnsureSuccessStatusCode();
-    }
-    
-    private record ImportImageRequest(string Path);
-    public async Task SendImageToHydrus(string path)
-    {
-        var response = await client.PostAsJsonAsync<ImportImageRequest>("add_files/add_file", new(path));
-        
+
         response.EnsureSuccessStatusCode();
     }
 
-    public async Task AddTagsToImage()
+    private class HydrusService
     {
-        throw new NotImplementedException();
+        [JsonPropertyName("name")] public string Name { get; set; }
+        [JsonPropertyName("type")] public int Type { get; set; }
+        [JsonPropertyName("type_pretty")] public string TypePretty { get; set; }
+        [JsonPropertyName("star_shape")] public string? StarShape { get; set; }
     }
 
-    public async Task AssociateUrlWithImage()
+    public async Task<Dictionary<string, string>> GetServices()
     {
-        throw new NotImplementedException();
+        var response = await client.GetAsync("get_services");
+
+        response.EnsureSuccessStatusCode();
+
+        var body = await response.Content.ReadAsStringAsync();
+
+        using var document = JsonDocument.Parse(body);
+        var root = document.RootElement;
+        var servicesElement = root.GetProperty("services");
+        var services = servicesElement.Deserialize<Dictionary<string, HydrusService>>();
+
+        return services.ToDictionary(s => s.Value.Name, s => s.Key);
+    }
+
+    private record ImportImageResult(int Status, string Hash, string Note);
+
+    public async Task<string> SendImageToHydrus(byte[] bytes)
+    {
+        client.DefaultRequestHeaders.Accept.Add(new("application/octet-stream"));
+        var byteContent = new ByteArrayContent(bytes);
+        byteContent.Headers.ContentType = new("application/octet-stream");
+
+        var response = await client.PostAsync("add_files/add_file", byteContent);
+
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<ImportImageResult>();
+
+        return result!.Hash;
+    }
+
+    public async Task AssociateUrlWithImage(string hash, IEnumerable<string> urls)
+    {
+        var response = await client.PostAsJsonAsync("add_urls/associate_url",
+            new
+            {
+                hash,
+                urls_to_add = urls
+            });
+
+        response.EnsureSuccessStatusCode();
+    }
+
+    public async Task AddTagsToImage(string hash, IEnumerable<string> tags, string service)
+    {
+        var serviceTags = new Dictionary<string, List<string>>
+        {
+            {
+                service, tags.ToList()
+            }
+        };
+
+        var body = new
+        {
+            hash,
+            service_keys_to_tags = serviceTags
+        };
+
+        var response = await client.PostAsJsonAsync("add_tags/add_tags", body);
+
+        response.EnsureSuccessStatusCode();
     }
 }
--- CivitaiFirehose/HydrusPusherBackgroundService.cs
+++ CivitaiFirehose/HydrusPusherBackgroundService.cs
@@ -4,16 +4,28 @@ namespace CivitaiFirehose;
 
 public class HydrusPusherBackgroundService(
     ChannelReader<ImageModel> channel,
+    IHttpClientFactory factory,
     HydrusClient client,
     ILogger<HydrusPusherBackgroundService> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         await client.VerifyAccess();
+        var services = await client.GetServices();
         
         await foreach (var image in channel.ReadAllAsync(stoppingToken))
         {
-            await client.SendImageToHydrus(image.ImageUrl);
+            var rawClient = factory.CreateClient();
+            
+            var bytes = await rawClient.GetByteArrayAsync(image.ImageUrl, stoppingToken);
+            
+            var hash = await client.SendImageToHydrus(bytes);
+
+            await client.AssociateUrlWithImage(hash, [image.ImageUrl]);
+
+            var service = services["my tags"];
+            
+            await client.AddTagsToImage(hash, ["civitai"], service);
         }
     }
 }
