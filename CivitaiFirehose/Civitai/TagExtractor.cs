9156b0f65c9dbf3c21ff797e0bb2c83b86a97498
jackkendall <jkendall3096@gmail.com>
2024-12-10 08:14:30 +0000

    Extract tag extraction to static

---
+++ CivitaiFirehose/Civitai/TagExtractor.cs
@@ -0,0 +1,48 @@
+namespace CivitaiFirehose;
+
+public static class TagExtractor
+{
+    public static List<string> GetTagsFromResponse(Items item)
+    {
+        var tags = new HashSet<string>
+        {
+            "source:civitai-firehose",
+            $"image id:{item.id}",
+            $"post id:{item.postId}",
+            $"nsfw level:{item.nsfwLevel}",
+            $"creator:{item.username}",
+            $"base model:{item.baseModel}"
+        };
+        
+        if (item.meta is null) return tags.ToList();
+
+        if (!string.IsNullOrWhiteSpace(item.meta.prompt))
+        {
+            var prompt = item.meta.prompt.Replace("\n", " ").Trim();
+            tags.Add($"prompt:{prompt}");
+
+            var promptedTags = prompt
+                .Split(",")
+                .Select(x =>
+                {
+                    // Gets rid of some syntax when invoking LORAs in prompts.
+                    x = x.Replace("<", string.Empty).Replace(">", string.Empty);
+                    return x.Trim();
+                })
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .ToList();
+            
+            foreach (var promptedTag in promptedTags)
+            {
+                tags.Add(promptedTag);
+            }
+        }
+
+        if (!string.IsNullOrWhiteSpace(item.meta.negativePrompt))
+        {
+            tags.Add($"negative prompt:{item.meta.negativePrompt.Replace("\n", " ").Trim()}");
+        }
+
+        return tags.ToList();
+    }
+}
--- CivitaiFirehose/ImageService.cs
+++ CivitaiFirehose/ImageService.cs
@@ -38,7 +38,7 @@ public class CivitaiPoller(CivitaiClient client, ILogger<CivitaiPoller> logger) 
             
             var postUrl = $"https://civitai.com/posts/{img.postId.ToString()}";
 
-            var tags = GetTagsFromResponse(img);
+            var tags = TagExtractor.GetTagsFromResponse(img);
             
             var image = new ImageModel(img.url, postUrl, tags);
 
@@ -56,51 +56,7 @@ public class CivitaiPoller(CivitaiClient client, ILogger<CivitaiPoller> logger) 
             if (t != null) await t;
         }
     }
-
-    private static List<string> GetTagsFromResponse(Items item)
-    {
-        var tags = new HashSet<string>
-        {
-            "source:civitai-firehose",
-            $"image id:{item.id}",
-            $"post id:{item.postId}",
-            $"nsfw level:{item.nsfwLevel}",
-            $"creator:{item.username}",
-            $"base model:{item.baseModel}"
-        };
-        
-        if (item.meta is null) return tags.ToList();
-
-        if (!string.IsNullOrWhiteSpace(item.meta.prompt))
-        {
-            var prompt = item.meta.prompt.Replace("\n", " ").Trim();
-            tags.Add($"prompt:{prompt}");
-
-            var promptedTags = prompt
-                .Split(",")
-                .Select(x =>
-                {
-                    // Gets rid of some syntax when invoking LORAs in prompts.
-                    x = x.Replace("<", string.Empty).Replace(">", string.Empty);
-                    return x.Trim();
-                })
-                .Where(s => !string.IsNullOrWhiteSpace(s))
-                .ToList();
-            
-            foreach (var promptedTag in promptedTags)
-            {
-                tags.Add(promptedTag);
-            }
-        }
-
-        if (!string.IsNullOrWhiteSpace(item.meta.negativePrompt))
-        {
-            tags.Add($"negative prompt:{item.meta.negativePrompt.Replace("\n", " ").Trim()}");
-        }
-
-        return tags.ToList();
-    }
-
+    
     public List<ImageModel> GetImages() => _images.ToList();
 
     public Func<int, Task>? NewImagesFound { get; set; }
