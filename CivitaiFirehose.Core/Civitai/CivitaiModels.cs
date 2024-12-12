325eb3385a0edcc849a43f7aa285fdc3b793bec3
jackkendall <jkendall3096@gmail.com>
2024-12-12 10:17:14 +0000

    Just remove the cursed meta fields altogether

--- CivitaiFirehose.Core/Civitai/CivitaiModels.cs
+++ CivitaiFirehose.Core/Civitai/CivitaiModels.cs
@@ -34,64 +34,8 @@ public record Stats(
 );
 
 public record Meta(
-    string Size,
-    long seed,
-    string Model,
-    int steps,
-    Hashes hashes,
     string prompt,
-    string Version,
-    string sampler,
-    string CFG_Scale,
-    Resources[] resources,
-    string Model_hash,
-    string negativePrompt,
-    bool nsfw,
-    bool draft,
-    Extra extra,
-    int width,
-    int height,
-    float? cfgScale,
-    int clipSkip,
-    int quantity,
-    string workflow,
-    string baseModel,
-    string Created_Date,
-    bool fluxUltraRaw,
-    CivitaiResources[] civitaiResources
-)
-{
-    /// <summary>
-    /// These are both extremely cursed in the Civitai JSON from time to time.
-    /// We don't care about them, so just ignore them.
-    /// </summary>
-    [JsonIgnore]
-    public int width { get; init; } = width;
-
-    [JsonIgnore]
-    public int height { get; init; } = height;
-}
-
-public record Hashes(
-    string model
-);
-
-public record Resources(
-    string hash,
-    string name,
-    string type
-);
-
-public record Extra(
-    int remixOfId
-);
-
-public record CivitaiResources(
-    string type,
-    int modelVersionId,
-    string modelVersionName,
-    float weight
-);
+    string negativePrompt);
 
 public record Metadata(
     string nextCursor,
