3bc7374858c90f6c5b261bca831535c7dd875e6b
jackkendall <jkendall3096@gmail.com>
2024-12-13 21:22:05 +0000

    Use proper event in image service

--- CivitaiFirehose.Core/ImageService.cs
+++ CivitaiFirehose.Core/ImageService.cs
@@ -6,8 +6,8 @@ namespace CivitaiFirehose;
 public class ImageService(BlacklistStore blacklist, IOptions<CivitaiSettings> options, ILogger<ImageService> logger)
 {
     private readonly BoundedQueue<ImageModel> _images = new(options.Value.QueryDefaults.Limit ?? 20);
-    
-    public Func<int, Task> NewImagesFound { get; set; } = _ => Task.CompletedTask;
+
+    public event Func<int, Task>? NewImagesFound;
     public IReadOnlyList<ImageModel> Images => _images.AsReadOnly();
     
     public async Task Enqueue(IEnumerable<ImageModel> images)
@@ -24,12 +24,17 @@ public class ImageService(BlacklistStore blacklist, IOptions<CivitaiSettings> op
             found++;
         }
 
-        if (found is 0) return;
-        
+        if (found is 0)
+        {
+            return;
+        }
+
         logger.LogInformation("Found {NewImages} new images", found);
 
-        // Tell the UI we have new images.
-        await NewImagesFound(found);
+        if (NewImagesFound is not null)
+        {
+            await NewImagesFound(found);
+        }
     }
 
     public async Task ClearAndEnqueue(IEnumerable<ImageModel> images)
