4da633d3b1eda54c2b1e1ea8494fe63501b3c377
jackkendall <jkendall3096@gmail.com>
2025-01-01 11:21:51 +0000

    Usings

--- CivitaiFirehose.Core/HomeViewmodel.cs
+++ CivitaiFirehose.Core/HomeViewmodel.cs
@@ -1,6 +1,5 @@
 using System.Threading.Channels;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace CivitaiFirehose;
 
--- CivitaiFirehose.Core/Hydrus/HydrusClient.cs
+++ CivitaiFirehose.Core/Hydrus/HydrusClient.cs
@@ -1,7 +1,6 @@
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using Microsoft.Extensions.Logging;
 
 namespace CivitaiFirehose;
 
